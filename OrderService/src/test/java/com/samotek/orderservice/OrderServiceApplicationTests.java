20ff97c23a232f98c5d6b4df3b716e14ffb293ec
smo <montassar.saber1@gmail.com>
2022-12-09 12:25:41 +0100

    add orderservice ms

---
+++ OrderService/src/test/java/com/samotek/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samotek.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
