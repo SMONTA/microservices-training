46b09681d101d5a11e69f806fb2c0b7e715ca8a3
smo <montassar.saber1@gmail.com>
2022-12-27 23:11:52 +0100

    add redis image/ add rate limiter in API gateway

--- OrderService/src/main/java/com/samotek/orderservice/external/client/PaymentService.java
+++ OrderService/src/main/java/com/samotek/orderservice/external/client/PaymentService.java
@@ -1,6 +1,8 @@
 package com.samotek.orderservice.external.client;
 
+import com.samotek.orderservice.exception.CustomException;
 import com.samotek.orderservice.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
  * @author Saber Montassar [montassar.saber1@gmail.com]
  * @date 12/25/22
  */
+@CircuitBreaker(name = "externalCircuitBreaker", fallbackMethod = "paymentFallback")
 @FeignClient(name="PAYMENT-SERVICE/payment/v1")
 public interface PaymentService {
 
   @PostMapping("/doPayment")
   ResponseEntity<Long> doPayment(@RequestBody PaymentRequest request);
+
+  default void paymentFallback(Exception e){
+    throw new CustomException("PAYMENT Service is not available", "PAYMENT_SERVICE_UNAVAILABLE", 500);
+  }
 }
--- OrderService/src/main/java/com/samotek/orderservice/external/client/ProductService.java
+++ OrderService/src/main/java/com/samotek/orderservice/external/client/ProductService.java
@@ -1,5 +1,7 @@
 package com.samotek.orderservice.external.client;
 
+import com.samotek.orderservice.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +12,15 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author Saber Montassar [montassar.saber1@gmail.com]
  * @date 12/20/22
  */
+@CircuitBreaker(name = "externalCircuitBreaker", fallbackMethod = "productFallback")
 @FeignClient(name="PRODUCT-SERVICE/products/v1/")
 public interface ProductService {
 
   @PutMapping("/reduceQuantity/{id}")
   ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId,
                                              @RequestParam long quantity);
+
+  default void productFallback(Exception e){
+    throw new CustomException("Product Service is not available", "PRODUCT_SERVICE_UNAVAILABLE", 500);
+  }
 }
